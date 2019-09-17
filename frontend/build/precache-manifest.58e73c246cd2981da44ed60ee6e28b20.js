34306d76f3cefd273a2e00c9561c7cf21358305b
Sergei Vatagin <s.vatagin@gmail.com>
2019-09-17 17:31:28 +0300

    initial commit

---
+++ frontend/build/precache-manifest.58e73c246cd2981da44ed60ee6e28b20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f94c7b090c05fa68da71ad18d60f9ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "004d8c217dd459b2c00a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "50092569e8b63addb95c",
+    "url": "/static/js/2.7c314d9f.chunk.js"
+  },
+  {
+    "revision": "004d8c217dd459b2c00a",
+    "url": "/static/js/main.1b4bb8f5.chunk.js"
+  },
+  {
+    "revision": "f4470a1d358b84166157",
+    "url": "/static/js/runtime~main.edc4bf8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
